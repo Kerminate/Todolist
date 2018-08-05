55fc6bc0dd8deaa26c84e2b6571e4ea1f6c256f6
Kerminate <kepeilin@outlook.com>
2018-08-05 14:08:44 +0800

    feat: init repo

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const auth = require('../controllers/user.js')
-const koaRouter = require('koa-router')
-
-const router = koaRouter()
-
-router.get('/user/:id', auth.getUserInfo) // 定义url的参数是id,用user的auth方法引入router
-router.post('/user', auth.postUserAuth)
-
-module.exports = router
